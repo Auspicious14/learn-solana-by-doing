f82211b800a1b483bb48f318eb651d7cfbe8ec1f
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-08-12 23:27:14 +0100

    Update index.tsx
    Update wallet balance checker

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,42 +4,55 @@ import {
   createSolanaRpc,
   createSolanaRpcSubscriptions,
   generateKeyPairSigner,
+  Address,
 } from "@solana/kit";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const rpc = createSolanaRpc("https://api.devnet.solana.com");
+  const rpc = createSolanaRpc("https://api.mainnnet-beta.solana.com");
   const rpcSubscriptions = createSolanaRpcSubscriptions(
-    "wss://api.devnet.solana.com"
+    "wss://api.mainnet-beta.solana.com"
   );
-  const [balance, setBalance] = useState<string>("");
+  const [walletBalance, setWalletBalance] = useState<number>();
+  const [message, setMessage] = useState<string>("")
+
 
   const generateWallet = async () => {
     return await generateKeyPairSigner();
   };
 
-  const handleWalletBalance = async () => {
+  const handleWalletBalance = async (address: Address) => {
     const wallet = await generateWallet();
     if (!wallet) {
       console.log("No wallet address");
-      alert("No wallet address");
+      setMessage("No wallet address");
       return;
     }
 
-    const balance = rpc.getBalance(wallet.address);
-    if (!balance.send()) {
-      console.log("No Balance found for this wallet");
+    const balance = await rpc.getBalance(wallet.address).send();
+    if (!balance.value) {
+      setMessage("No Balance found for this wallet");
       return;
     }
-    // setBalance(balance)
+    
+    setWalletBalance(balance.value / 1_000_000_000)
+    setMessage(`You have ${walletBalance} balance left.`)
   };
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <input type="text" placeholder="Add a wallet address" />
+      <input 
+        type="text" 
+        placeholder="Add a wallet address" 
+        className="p-2 rounded-sm outline-none border border-gray-200 focus:ring-blue-500"
+        onChange={(e) => handleWalletBalance(e.target.value)}
+      />
+      {message && (
+       <div className="py-4 flex justify-center items-center">{message}</div>
+      )}
     </main>
   );
 }
