2a2c9fadf0d22f0d7dc14ed06c77923be74033c6
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-08-13 12:54:45 +0100

    Update context.tsx

--- src/provider/context.tsx
+++ src/provider/context.tsx
@@ -12,10 +12,11 @@ import {
   WalletProvider,
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { createContext, useContext } from "react";
+import { createContext, useContext, useMemo } from "react";
+import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adapter-wallets";
 
 interface ISolanaState {
-  rpc: Rpc<SolanaRpcApiMainnet>; // Limit the API to only those methods found on Mainnet (ie. not `requestAirdrop`)
+  rpc: Rpc<SolanaRpcApiMainnet>;
   rpcSubscriptions: RpcSubscriptions<SolanaRpcSubscriptionsApi>;
 }
 
@@ -23,7 +24,7 @@ const SolanaContext = createContext<ISolanaState | undefined>(undefined);
 
 export const useSolanaState = () => {
   const context = useContext(SolanaContext);
-  if (!context || context === undefined) {
+  if (!context) {
     throw Error("Solana Provider must be used within the app provider");
   }
   return context;
@@ -33,16 +34,19 @@ interface IProps {
   children: React.ReactNode;
 }
 
-export const SolanaContextProvider: React.FC<IProps> = async ({ children }) => {
-  const rpc = createSolanaRpc(mainnet("https://api.mainnnet-beta.solana.com"));
-  const rpcSubscriptions = createSolanaRpcSubscriptions(
-    "wss://api.mainnet-beta.solana.com"
+export const SolanaContextProvider: React.FC<IProps> =  ({ children }) => {
+  const endpoint = "https://api.mainnet-beta.solana.com"
+  const rpc = createSolanaRpc(mainnet(endpoint));
+  const rpcSubscriptions = createSolanaRpcSubscriptions(endpoint.replace("https, wss"));
+  
+const wallets = useMemo( () => 
+  [new PhantomWalletAdapter(), new SolflareWalletAdapter()], []
   );
-
+  
   return (
     <SolanaContext.Provider value={{ rpc, rpcSubscriptions }}>
-      <ConnectionProvider endpoint={""}>
-        <WalletProvider wallets={[]}>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets}>
           <WalletModalProvider>{children}</WalletModalProvider>
         </WalletProvider>
       </ConnectionProvider>
