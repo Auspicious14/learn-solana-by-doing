d34bf4e01e2bfb2d29b09c7ffe0282d9d6f26a8f
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-08-13 17:27:36 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SolanaContextProvider } from '@/provider/context';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SolanaContextProvider>
+     <Component {...pageProps} />
+  </SolanaContextProvider>
+  )
 }
