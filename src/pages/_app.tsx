078972e43d429169a71362d4755433578c20fb13
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-08-13 18:07:38 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SolanaContextProvider } from '@/provider/context';
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
