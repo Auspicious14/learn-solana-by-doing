8124157edde490cc75330715a36db917810af5ad
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-08-20 15:42:04 +0100

    Create type for NFT

---
+++ src/modules/ntf-viewer/model.ts
@@ -0,0 +1,21 @@
+interface INFT {
+  id: string;
+  name: string;
+  symbol?: string;
+  description?: string;
+  image?: string;
+  collection?: string;
+  attributes: Array<{
+    trait_type: string;
+    value: string | number;
+  }>;
+  uri?: string;
+  creators: Array<{
+    address: string;
+    verified: boolean;
+    share: number;
+  }>;
+  royalty?: {
+    basis_points: number;
+  };
+}
