9b35d7730bf3a21d7ed8afc8a7f933490497973b
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-08-20 18:06:54 +0100

    refactor: rename UI component to BalanceCheckerPage

--- src/modules/wallet-balance/page.tsx
+++ src/modules/wallet-balance/page.tsx
@@ -4,7 +4,7 @@ import { useBalanceState } from "./context";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { useWalletState } from "@/provider/walletContext"
 
-export const BalanceChecker = () => {
+export const BalanceCheckerPage = () => {
   const { 
     publicKey, 
     connected,
